136d1ba6d4d7d48a88c7b7c00bc5ab6b571e1cfa
Naim Gurleyuk <naimgur@gmail.com>
2025-01-31 21:41:47 +0300

    uploads folder creation if not exists implemented

--- src/FileStore/LocalFileStore.ts
+++ src/FileStore/LocalFileStore.ts
@@ -2,35 +2,65 @@ import { Injectable } from '@nestjs/common';
 import { FileStore } from './index';
 import * as fs from 'fs';
 import * as util from 'util';
+import * as path from 'path';
 
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
+const mkdir = util.promisify(fs.mkdir);
 
 @Injectable()
 export class LocalFileStore implements FileStore {
+  private readonly uploadDir = 'uploads';
+  private readonly filePath = path.join(this.uploadDir, 'transactionFile.csv');
+
+  constructor() {
+    this.ensureUploadsDirExists();
+  }
+
+  private async ensureUploadsDirExists(): Promise<void> {
+    try {
+      await mkdir(this.uploadDir, { recursive: true });
+    } catch (error) {
+      console.error('Failed to create uploads directory:', error);
+      throw error;
+    }
+  }
+
   async writeFile(file: Buffer): Promise<void> {
-    await writeFile(`uploads/transactionFile.csv`, file);
+    await this.ensureUploadsDirExists();
+    await writeFile(this.filePath, file);
   }
 
   async readFile(): Promise<Record<string, string>[]> {
-    const filePath = 'uploads/transactionFile.csv';
-    const fileContent = await readFile(filePath, 'utf-8');
+    await this.ensureUploadsDirExists();
 
-    const lines = fileContent.split('\n').filter((line) => line.trim() !== '');
+    try {
+      const fileContent = await readFile(this.filePath, 'utf-8');
 
-    const headers = lines[0].split(',').map((header) => header.trim());
+      const lines = fileContent
+        .split('\n')
+        .filter((line) => line.trim() !== '');
 
-    const result = lines.slice(1).map((line) => {
-      const values = line.split(',').map((value) => value.trim());
-      const row: Record<string, string> = {};
+      const headers = lines[0].split(',').map((header) => header.trim());
 
-      headers.forEach((header, index) => {
-        row[header] = values[index] || '';
+      const result = lines.slice(1).map((line) => {
+        const values = line.split(',').map((value) => value.trim());
+        const row: Record<string, string> = {};
+
+        headers.forEach((header, index) => {
+          row[header] = values[index] || '';
+        });
+
+        return row;
       });
 
-      return row;
-    });
-
-    return result;
+      return result;
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.error('File not found:', this.filePath);
+        return []; // Return an empty array if the file doesn't exist
+      }
+      throw error;
+    }
   }
 }
